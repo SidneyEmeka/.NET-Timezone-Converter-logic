6651c71898892ea316a9f980b5b8b357fa0546e3
Sidswipe <sidswipexyz@gmail.com>
2024-03-26 11:40:53 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,13 +5,11 @@ void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
 {
     int diff = 0;
     if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12) //GMT cannot be more than 12
-    
         Console.WriteLine("Invalid GMT");
     
     else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
-    {
         diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-    }
+    
     else
     {
         diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
